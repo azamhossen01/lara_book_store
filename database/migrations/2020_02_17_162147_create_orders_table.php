15245a9743b591063f15d8e3fb13cc82cff9231a
Azgar_lappy <azamhossen01@gmail.com>
2020-02-23 21:56:29 +0600

    conflict

--- database/migrations/2020_02_17_162147_create_orders_table.php
+++ database/migrations/2020_02_17_162147_create_orders_table.php
@@ -17,13 +17,9 @@ class CreateOrdersTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('customer_id');
             $table->unsignedBigInteger('shipping_id');
-<<<<<<< HEAD:database/migrations/2020_02_17_102457_create_orders_table.php
-            $table->float('total');
-=======
             $table->float('total')->nullable();
             $table->integer('payment_method')->comment('0=cash on delivery, 1=bkash payment'); 
             $table->string('transaction_id')->nullable();
->>>>>>> 46a22ee76f3042288a7e4a17f16eb2fc41dd0c87:database/migrations/2020_02_17_162147_create_orders_table.php
             $table->integer('status')->default(0);
             $table->timestamps();
         });
